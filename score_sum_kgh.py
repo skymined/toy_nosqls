a52bf666e1bd9b91a2eb29a41c19feaa5f1971e4
rugkfl <kimgyoungha61@gmail.com>
2023-12-19 20:08:04 +0900

    과제 실습

--- score_sum_kgh.py
+++ score_sum_kgh.py
@@ -13,25 +13,59 @@ problem_answer = list(coll_problem_answer.find({}))
 user = list(coll_user.find({}))
 user_answer = list(coll_user_answer.find({}))
 
+# 각 문항 정답 출력에 대한 구문
+print("-------------------")
+print("각 문항 정답 : ", end="")
+for i in range(len(problem)):
+    if i < 2 :
+        print("{}".format(problem[i]['correct_answer']), end=",")
+    else :
+        print("{}".format(problem[i]['correct_answer']))
+    pass
+print("")
+score_num_list=[]
+print("응시자별 채점 결과 :")
+sum_score=0
 for i in range(len(user)) :
-    print("{}".format(user[i]))
-    for j in range(len(problem)) :
-        print("{}".format(problem[j]))
+    user_answer_list=[]
+    correct_num_list=[]
+    score_list=[]
+    score=0
+    
+    print("{}".format(user[i]["user_name"])) #user에 있는 user_name을 출력
+
+    for j in range(len(user_answer)):
+        if  user[i]['_id'] == user_answer[j]['user_id']:
+            user_answer_list.append(user_answer[j]['user_answer'])
+            # print(user_answer_list)
+    for j in range(len(problem)):        
+            correct_num_list.append(problem[j]['correct_answer'])
+            score_list.append(problem[j]['score'])
+            # print(correct_num_list)
+    for j in range(len(user_answer_list)):        
+        if user_answer_list[j] == correct_num_list[j]:
+            score += score_list[j]
+            
+            pass
+        pass
+    score_num_list.append(score)
+    print(score)
+    print(score_num_list)
+    
+
+print("과목 평균 점수: ", end="")
+for i in range(len(score_num_list)):
+    sum_score += score_num_list[i]
+    pass
+
+print(sum_score/len(user))
 
 
 
 
-# problem = coll_problem.find()
-
-
-# for i in range(user_id_list):
-
-#     for j in range(problem_id_list):
 
 
 
-#         user_answer = []
 
-#         if user_answer[1] == answer[1] : 
-#             coll_user_answer.update_one( { }, "$set":"user_score":score[1])
+
    
