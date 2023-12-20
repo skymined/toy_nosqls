3cd68c1386bc37f32ca41bb6b008c8d3d7b02af6
rugkfl <kimgyoungha61@gmail.com>
2023-12-20 11:11:39 +0900

    주석

--- score_sum_kgh.py
+++ score_sum_kgh.py
@@ -1,13 +1,14 @@
 from pymongo import MongoClient
 mongoClient = MongoClient("mongodb://192.168.0.145:27017")    # mongodb 접속
 database = mongoClient["toy_nosqls"]   # database 연결
-# database[collection_name]        # collection 작업
 
+# 각 collection에 대해 변수 선언
 coll_problem_list = database['problem_list']
 coll_problem_answer = database['problem_answer']
 coll_user = database['user']
 coll_user_answer = database['user_answer']
 
+# 각 collection find하여 변수 선언
 problem = list(coll_problem_list.find({}))
 problem_answer = list(coll_problem_answer.find({}))
 user = list(coll_user.find({}))
@@ -25,39 +26,37 @@ def answer_data():
 
 
 def score_result() :
-    score_num_list=[]
+    score_num_list=[] # 점수에 대한 리스트
     print("응시자별 채점 결과 :")
-    sum_score=0
-    for i in range(len(user)) :
+    sum_score=0 # 점수 합산을 위해 0으로 설정
+    for i in range(len(user)) : # 참여자의 수만큼
         user_answer_list=[]
         correct_num_list=[]
         score_list=[]
         score=0
 
-        for j in range(len(user_answer)):
-            if  user[i]['_id'] == user_answer[j]['user_id']:
+        for j in range(len(user_answer)): # user_answer의 길이만큼
+            if  user[i]['_id'] == user_answer[j]['user_id']: # user의 id와 user_answer의 user_id와 같을시
                 user_answer_list.append(user_answer[j]['user_answer'])
-                # print(user_answer_list)
-        for j in range(len(problem)):        
+        for j in range(len(problem)): # problem_list 길이만큼    
                 correct_num_list.append(problem[j]['correct_answer'])
                 score_list.append(problem[j]['score'])
-                # print(correct_num_list)
-        for j in range(len(user_answer_list)):        
-            if user_answer_list[j] == correct_num_list[j]:
-                score += score_list[j]
-                coll_user_answer.update_one({"user_id":user[j]['_id'] , "problem_id" : problem[j]}, {"$set":{"user_score":problem[j]['score']}})
+        for j in range(len(user_answer_list)): #user_anwer_list의 길이만큼       
+            if user_answer_list[j] == correct_num_list[j]: # 참여자의 입력답과 정답이 같을 시
+                score += score_list[j]  # 점수 배점
+                coll_user_answer.update_one({"user_id":user[j]['_id'] , "problem_id" : problem[j]}, {"$set":{"user_score":problem[j]['score']}}) # user_score에 score update
             else :
                 coll_user_answer.update_one({"user_id":user[j]['_id'] , "problem_id" : problem[j]}, {"$set":{"user_score":0}})      
             pass    
-
-        score_num_list.append(score)
+        
+        score_num_list.append(score) # 각 점수를 score_num_list에 넣기
         print("{} : {}점".format(user[i]["user_name"], score)) #user에 있는 user_name을 출력
-
-    for i in range(len(score_num_list)):
-        sum_score += score_num_list[i]
+    
+    for i in range(len(score_num_list)):  # 각 점수를 합산
+        sum_score += score_num_list[i] 
         pass
 
-    print("과목 평균 점수: {}점".format(sum_score/len(user)))
+    print("과목 평균 점수: {}점".format(sum_score/len(user))) # 전체 합산 점수/사용자의 수만큼 해서 전체 평균 구하기
 
 
 
